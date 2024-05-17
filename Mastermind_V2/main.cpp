30fa183675691de3bb7740dc82c245cb56cbe07b
Julian Vara <jvara04@outlook.com>
2024-05-16 20:25:06 -0700

    Finished Mastermind 1a lab Assignment

--- Mastermind_V2/main.cpp
+++ Mastermind_V2/main.cpp
@@ -16,83 +16,65 @@ using namespace std;
 
 string generatedArr(int);                            // This functions creates the generated Arr that will have the code the user has to guess 
 bool compareArr(string, string, int, int &, int &);
-string AI();
-
+string AI(int);
+void display(int, int, int [], int []);
+void counter(int, int, int [], int []);
 
 
 int main()
-{   
-    
+{
     const int len = 4; 
-    int counterRight[len];
-    int counterWrong[len];
-    int right, rightWrong; 
     bool dup;
-    string code, guess; 
     int tries = 10;
     int amCor = 0;      // amCor = right # wrong place
-    int coSpot = 0;     // coSpot = correct spot   
+    int coSpot = 0;     // coSpot = correct spot  
+    string guess, code, userCode; 
+    int choice = 0; 
+    int rightArr[5] = {0, 0, 0, 0, 0};
+    int wrongArr[5] = {0, 0, 0, 0, 0};
 
     srand(time(NULL));              // Generates Random Number 
 
-    code = generatedArr(len);
-          // Generates a random Array that will be compared to the input array to win  
-  
+   
+    code = generatedArr(len);             // Generates a random Array that will be compared to the input array to win  
+    
+    cout << "Hello User this program will guess a code by going from 0000-9999 " << endl;
+    cout << "1) Random Code " << endl;
+    cout << "2) User Code " << endl; 
+    cin >> choice;
+
+    if (choice == 1)
+    {
         cout << "Code Guess #right #right in wrong spot Sum" << endl; 
         for (int i = 0; i < 10000; i++)
         {
-            guess = AI(); 
-            
+            guess = AI(len); 
             compareArr(code, guess, len, amCor, coSpot);
             cout << code << " " << guess << "      " << coSpot << "          " << amCor << "             " << amCor + coSpot << endl;
-                if (coSpot == 1)
-                {
-                    counterRight[0]++; 
-                } else if (coSpot == 2)
-                {
-                    counterRight[1]++;
-                } else if (coSpot == 3)
-                {
-                    counterRight[2]++;
-                } else if (coSpot == 4)
-                {
-                    counterRight[3]++;
-                }
-
-                if (amCor == 1)
-                {
-                    counterWrong[0]++;
-                } else if (amCor == 2)
-                {
-                    counterWrong[1]++;
-                } else if (amCor == 3)
-                {
-                    counterWrong[2]++;
-                } else if (amCor == 4)
-                {
-                    counterWrong[3]++;
-                }
-                
-                
+            counter(coSpot, amCor, rightArr, wrongArr); 
         }
-
-        for (int i = 0; i < len; i++)
+        display(coSpot, amCor, rightArr, wrongArr); 
+    } else if (choice == 2)
+    {
+        cout << "Please enter Code on this line: ";
+        cin >> userCode; 
+        cout << "Code Guess #right #right in wrong spot Sum" << endl; 
+        for (int i = 0; i < 10000; i++)
         {
-            cout << "Number Right in Position: " << counterRight[i] << endl;
+            guess = AI(len); 
+            compareArr(userCode, guess, len, amCor, coSpot);
+            cout << userCode << " " << guess << "      " << coSpot << "          " << amCor << "             " << amCor + coSpot << endl;
+            counter(coSpot, amCor, rightArr, wrongArr); 
         }
+        display(coSpot, amCor, rightArr, wrongArr); 
+    }
 
-        for (int i = 0; i < len; i++)
-        {
-            cout << "Number Right Wrong in Position: " << counterWrong[i] << endl;
-        }
-
-
-     
+    
 }
+
 
 string generatedArr(int len)
 {   
-   
     string code = "0000";
     for (int i = 0; i < len; i++)       // for loop generates a random number 
     {
@@ -101,8 +83,7 @@ string generatedArr(int len)
     return code; 
 }
 
-
-bool compareArr(string code, string sGuess, int len, int &amCor, int &coSpot)
+bool compareArr(string code, string guess, int len, int &amCor, int &coSpot)
 {
      
     amCor = 0;      // These two integer functions need to be reset everytime the function is called 
@@ -112,10 +93,10 @@ bool compareArr(string code, string sGuess, int len, int &amCor, int &coSpot)
 
     for(int i = 0; i < len; i++)
     {
-        if(sGuess[i] == code[i])
+        if(guess[i] == code[i])
         { 
             check[i] = 'x';
-            sGuess[i] = 'x';
+            guess[i] = 'x';
             coSpot++; 
         }
     }
@@ -123,7 +104,7 @@ bool compareArr(string code, string sGuess, int len, int &amCor, int &coSpot)
     {
         for(int j = 0; j < len; j++)
         {
-            if((i != j) && (code[j] == sGuess[i]) && (check[j] == ' '))
+            if((i != j) && (code[j] == guess[i]) && (check[j] == ' '))
             {
                 check[j] = 'x';
                 amCor ++; 
@@ -141,12 +122,11 @@ bool compareArr(string code, string sGuess, int len, int &amCor, int &coSpot)
    
 }
 
-
-string AI()
+string AI(int len)
 {
     static int guess=-1;
-    guess++;
-    string sGuess="0000";
+    guess++; 
+    string sGuess = "0000";
     int n1000=(guess-guess%1000)/1000;
     int n100=(guess%1000-guess%100)/100;
     int n10=(guess%100-guess%10)/10;
@@ -159,3 +139,53 @@ string AI()
     return sGuess;
 }
 
+void counter(int coSpot, int amCor, int rightArr[], int wrongArr[])
+{
+    if (coSpot == 0)
+    {
+        rightArr[0]++;
+    } else if (coSpot == 1)
+    {
+        rightArr[1]++;
+    } else if (coSpot == 2)
+    {
+        rightArr[2]++;
+    } else if (coSpot == 3)
+    {
+        rightArr[3]++; 
+    } else if (coSpot == 4)
+    {
+        rightArr[4]++;
+    }
+
+    if (amCor == 0)
+    {
+        wrongArr[0]++;
+    } else if (amCor == 1)
+    {
+        wrongArr[1]++;
+    } else if (amCor == 2)
+    {
+        wrongArr[2]++;
+    } else if (amCor == 3)
+    {
+        wrongArr[3]++; 
+    } else if (amCor == 4)
+    {
+        wrongArr[4]++;
+    }
+        
+}
+
+void display(int coSpot, int amCor, int rightArr[], int wrongArr[])
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "The amount RIGHT in position " << i << ": " << rightArr[i] << endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "The amount RIGHT in WRONG in position " << i << ": " << wrongArr[i] << endl;
+    }
+}
